6a26d567188fa5e2176f8053b2868df739d7a18f
Marina Cassis <marinacassis@gmail.com>
2016-10-25 20:17:04 -0200

    Versão 2

--- app/src/main/java/br/com/marina/feiradosimportados/activities/Cadastro.java
+++ app/src/main/java/br/com/marina/feiradosimportados/activities/Cadastro.java
@@ -1,13 +1,15 @@
-package br.com.marina.feiradosimportados;
+package br.com.marina.feiradosimportados.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import br.com.marina.feiradosimportados.R;
+
+public class Cadastro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_cadastro);
     }
 }
